cedd2cba5947b308b5ff7bad356b2a694324319d
Oliver George <oliver@condense.com.au>
2020-09-05 14:43:26 +1000

    working

--- app/src/app/core.cljs
+++ app/src/app/core.cljs
@@ -1,7 +1,21 @@
 (ns app.core
-  (:require [react]
+  (:require [goog.dom :as gdom]
+            [reagent.dom :as dom]
+            [react]
             [caniusestories]))
 
 (.log js/console react/Component)
 (.log js/console caniusestories)
+
+(defn simple-component []
+  [:div
+   [:p "I am a component!"]
+   [:> caniusestories/Button.Button 
+    {:primary true
+     :label "Roar"}]
+   [:p.someclass
+    "I have " [:strong "bold"]
+    [:span {:style {:color "red"}} " and red "] "text."]])
+
+(dom/render [simple-component] (gdom/getElement "app"))
 
--- uilib/src/index.js
+++ uilib/src/index.js
@@ -1,3 +1,3 @@
 import * as Button from "./stories/Button";
 
-export default Button;
+export {Button};
