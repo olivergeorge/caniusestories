d0c5c7099d270c0e26c98610f8c95cd43fceec84
Oliver George <oliver@condense.com.au>
2020-09-09 21:50:27 +1000

    update webpack to consume js & css

---
+++ app/webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+    },
+};
